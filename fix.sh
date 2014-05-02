edbccbdc1defe1b6deccf3e53e055145205ccbef
Pedro Felipe Gomes Teles <pedrofelipegt@gmail.com>
2014-05-02 16:23:52 -0300

    Arquivo shellScript para ajuste no git quando estiver usando outro computador

--- fix.sh
+++ fix.sh
@@ -1,1 +1,3 @@
 git remote set-url origin https://pedroteles@github.com/pedroteles/manutencao.git
+git config --global user.name "Pedro Felipe Gomes Teles"
+git config --global user.email pedrofelipegt@gmail.com
